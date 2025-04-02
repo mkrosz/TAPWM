c4b249492b0e6bc5ee33c33741c59a3da13482f6
MIKE WILLY FRANGUELLI RODRIGUES <0031432512022@lti.fatecso.edu.br>
2025-04-02 18:09:02 -0300

    exercicio 4 dia 02-04-2025

---
+++ Atividades/ATV 4 - variaveis/script.js
@@ -0,0 +1,10 @@
+
+resultNome = prompt("Qual é o seu nome?");
+resultNota1 = prompt("Digite sua primeira nota:");
+resultNota2 = prompt("Digite sua segunda nota:");
+resultNota3 = prompt("Digite sua terceira nota:");
+
+
+resultadoFinal = resultNome + parseFloat(resultNota1) + parseFloat(resultNota2) + parseFloat(resultNota3);
+
+return resultadoFinal;
